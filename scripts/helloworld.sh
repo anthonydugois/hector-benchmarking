24e3f0d55eadb863dbe29f57981b9cd8f2f1a71b
Anthony Dugois <anthony.dugois@protonmail.com>
2023-04-27 10:46:34 +0200

    fix input/output paths

--- scripts/helloworld.sh
+++ scripts/helloworld.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-INPUT_FILE=/usr/src/app/experiment/input/helloworld.csv
+INPUT_FILE=experiment/input/helloworld.csv
 JOB_NAME=helloworld
-OUTPUT_DIR=/usr/src/app/experiment/output/helloworld
+OUTPUT_DIR=experiment/output/helloworld
 
 ./scripts/run.sh $INPUT_FILE --job-name $JOB_NAME --site $1 --cluster $2 --start-index $3 --walltime $4 --output $OUTPUT_DIR --log log
 
-./scripts/tidy.sh $OUTPUT_DIR --archive
+if [ -d $OUTPUT_DIR ]
+then
+  ./scripts/tidy.sh $OUTPUT_DIR --archive
+else
+  echo "Error: $OUTPUT_DIR does not exist."
+fi
